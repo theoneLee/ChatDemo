aef662281a8e1e86d2f48b060f190c907a6e9fcb
theoneLee <623507020@qq.com>
2019-08-23 00:34:17 +0800

    add server basic framework

---
+++ src/main/java/attribute/Attributes.java
@@ -0,0 +1,8 @@
+package attribute;
+
+import io.netty.util.AttributeKey;
+import session.Session;
+
+public interface Attributes {
+    AttributeKey<Session> SESSION = AttributeKey.newInstance("session");
+}
